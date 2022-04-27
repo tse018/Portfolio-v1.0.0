8f0fd15bf1605a7920dd03ece1d5a92eb5162f40
tse018 <60438717+tse018@users.noreply.github.com>
2022-04-27 18:26:00 +0200

    Setting up sanity based on database model

--- tema-8/project/app/src/components/Header.vue
+++ tema-8/project/app/src/components/Header.vue
@@ -1,9 +1,91 @@
 <template>
+   <header class="header-container">
+      <section class="header-container__logo">
+         <RouterLink :to="{name: 'home'}">
+            <img src="/images/favicon-32x32.png" alt="page logo"/>
+         </RouterLink>
+      </section>
+
+      <nav class="header-container__desktop">
+         <ul class="header-container__desktop-elements">
+            <li class="header-container__desktop-element">
+               About 
+            </li>
+
+            <li class="header-container__desktop-element">
+               Education
+            </li>
+
+            <li class="header-container__desktop-element">
+               Projects
+            </li>
+
+            <li class="header-container__desktop-element">
+               Contact
+            </li>
+      
+            <li class="header-container__desktop-element">
+               Resume
+            </li>
+         </ul>
+      </nav>
+
+      <nav class="header__container-mobile">
+         <MobileMenu :icon="'menuOpen'" />
+      </nav>
+   </header>
 </template>
 
 <script>
+import MobileMenu from '../components/MobileMenu.vue';
+
+export default {
+   components: {
+      MobileMenu,
+   }
+}
 </script>
 
 <style>
+.header-container {
+   position: relative;
+   
+   height: 50px;
+}
 
+.header-container__desktop-elements {
+   display: flex;
+   padding: 0 15px 0 15px;
+}
+
+@media screen and (max-width: 600px) {
+   .header__container-mobile {
+      display: block;
+   }
+
+   .header-container__desktop {
+      display: none;
+   }
+}
+
+@media screen and (min-width: 601px) {
+   .header__container-mobile {
+      display: block;
+   }
+
+   .header-container__desktop {
+      display: none;
+   }
+}
+
+@media screen and (min-width: 1200px) {
+   .header__container-mobile {
+      display: none;
+   }
+
+   .header-container__desktop {
+      display: block;
+   }
+}
 </style>
+
---
+++ tema-8/project/studio/schemas/documents/education.js
@@ -0,0 +1,40 @@
+export default {
+   title: 'Education',
+   name: 'education',
+   type: 'document',
+   fields: [
+      {
+         title: 'Institute',
+         name: 'institute',
+         type: 'string',
+      },
+      {
+         title: 'Study program',
+         name: 'studyProgram',
+         type: 'string',
+      },
+      {
+         title: 'Courses',
+         name: 'courses',
+         type: 'string',
+      },
+      {
+         title: 'Start date',
+         name: 'startDate',
+         type: 'date',
+         options: {
+            dateFormat: 'DD-MMM-YYYY',
+            calenderTodayLabel: 'Today'
+         }
+      },
+      {
+         title: 'End date',
+         name: 'endDate',
+         type: 'date',
+         options: {
+            dateFormat: 'DD-MMM-YYYY',
+            calenderTodayLabel: 'Today'
+         }
+      },
+   ]
+}
