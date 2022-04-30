cd2a966b408e08a8962a03b7fcdc75c127ad8e77
tse018 <60438717+tse018@users.noreply.github.com>
2022-04-30 19:38:45 +0200

    Footer

--- tema-8/project/app/src/components/Footer.vue
+++ tema-8/project/app/src/components/Footer.vue
@@ -1,4 +1,9 @@
 <template>
+  <footer class="footer-container">
+    <p class="footer-container__content">
+      Designed & Built by Thanushan Sellathurai
+    </p>
+  </footer>
 </template>
 
 <script>
@@ -8,5 +13,17 @@ export default {
 </script>
 
 <style>
+.footer-container {
+  position:absolute;
+  bottom:0;
+  width:100%;
+  height:60px;   /* Height of the footer */
+  background:#6cf;
+}
+
+.footer-container__content {
+  margin-top: 15px;
+  text-align: center;
+}
 
 </style>
