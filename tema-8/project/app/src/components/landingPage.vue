dc6b025ccaabcd8f24d1cb755bdca66078853df9
tse018 <60438717+tse018@users.noreply.github.com>
2022-05-05 23:36:46 +0200

    working progress on mousemove effect

--- tema-8/project/app/src/components/landingPage.vue
+++ tema-8/project/app/src/components/landingPage.vue
@@ -1,16 +1,15 @@
 <template>
-   <section class="section-introduction">
-      <h1 class="section-introduction__name">
+   <section ref="container" @mousemove="mouseMove" class="section-introduction">
+      <h1 class="section-introduction__name" :style="{style}">
+         {{ style }}
          THANUSHAN SELLATHURAI
       </h1>
-      
+
       <h2 class="section-introduction__work-title">
          FRONT-END DEVELOPER
       </h2>
 
-      <h2 class="section-introduction__portfolio">
-         PORTFOLIO
-      </h2>
+      <h2 class="section-introduction__portfolio">PORTFOLIO</h2>
 
       <a href="https://github.com/tse018" target="_blank" class="section-introduction__github">
          Check out my Github!
@@ -23,31 +22,62 @@
 </template>
 
 <script>
-import Icons from '../components/Icons.vue';
+import Icons from "../components/Icons.vue";
 
 export default {
-   components: {
-      Icons,
+   data() {
+      return {
+         walk: 100,
+         xWalk: null,
+         yWalk: null,
+      };
    },
 
-   async created() {
-      await this.$store.dispatch('fectchSanityData');
-      this.$store.state.data;
+   components: {
+      Icons,
    },
 
    computed: {
       data() {
          return this.$store.getters.getData;
-      }
+      },
+
+      style() {
+         return {
+            textShadow: `'${this.xWalk}px' + ${this.yWalk}px + #FFFFF'`
+         
+         }
+      },
    },
 
    methods: {
       scrollTo() {
-         const element = document.getElementById('about');
-         element.scrollIntoView({ behavior: 'smooth' });
+         const element = document.getElementById("about");
+         element.scrollIntoView({ behavior: "smooth" });
       },
-   }
-}
+
+      mouseMove(e) {
+         const width = this.$refs['container'].offsetWidth;
+         const heigth = this.$refs['container'].offsetHeight;
+
+         let x = e.offsetX;
+         let y = e.offsetY;
+         //console.log(x, y)
+         
+         if(this !== e.target) {
+            x = x + e.target.offsetLeft;
+            y = y + e.target.offsetTop;
+            //console.log(x,y)
+         }
+
+         this.xWalk= Math.round((x / width * this.walk) - (this.walk / 2));
+         this.yWalk = Math.round((y / heigth * this.walk) - (this.walk / 2));
+         console.log(this.xWalk, this.yWalk)
+         //console.log(this.xWalk)
+         //console.log(this.yWalk)
+      },
+   },
+};
 </script>
 
 <style scoped>
@@ -58,7 +88,7 @@ export default {
       grid-template-columns: repeat(4, 1fr);
       gap: 20px;
    }
-   
+
    .section-introduction__name {
       grid-column: 1 / 5;
       grid-row: 5;
@@ -84,7 +114,7 @@ export default {
       grid-template-columns: repeat(4, 1fr);
       gap: 20px;
    }
-   
+
    .section-introduction__name {
       grid-column: 1 / 5;
       grid-row: 5;
@@ -128,30 +158,43 @@ export default {
    .section-introduction__portfolio {
       grid-column: 11;
       grid-row: 8;
+      animation: color-change 3s infinite;
+      font-size: var(--desktop-font-size-undertitle);
+   }
+
+   @keyframes color-change {
+      0% {
+         color: white;
+      }
+      50% {
+         color: red;
+      }
+      100% {
+         color: aqua;
+      }
    }
 
    .section-introduction__github {
       grid-column: 3 / 7;
       grid-row: 5;
-      border: 2px solid aqua;
+      border: 2px solid var(--font-color-highligth);
       height: 100px;
-      color: aqua;
-      padding: 25px 0 0 0; 
-      opacity: 0.6;
-      transition: 0.3s;
+      color: var(--font-color-highligth);
+      padding: 25px 0 0 0;
+      transition: 0.5s;
       text-align: center;
       text-decoration: none;
    }
 
-   .section-introduction__github:hover {
-      opacity: 1;
-      background-color: aquamarine;
+   .section-introduction__github:hover,
+   .section-introduction__github:focus {
+      box-shadow: inset 14em 0 0 0 var(--font-color-highligth);
       color: black;
    }
 
    .section-introduction__left-arrow {
       grid-column: 12;
-      grid-row: 4; 
+      grid-row: 4;
    }
 }
 </style>
